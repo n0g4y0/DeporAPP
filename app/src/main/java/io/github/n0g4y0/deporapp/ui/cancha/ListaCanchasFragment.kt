422717b0edad33973ecd6d132794665d2263ad52
n0g4y0 <n0g4y0@gmail.com>
2019-11-28 11:08:37 -0400

    trabajando en el recyclerView y el grafico de navegacion

--- app/src/main/java/io/github/n0g4y0/deporapp/ui/cancha/ListaCanchasFragment.kt
+++ app/src/main/java/io/github/n0g4y0/deporapp/ui/cancha/ListaCanchasFragment.kt
@@ -9,109 +9,26 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import io.github.n0g4y0.deporapp.R
+import io.github.n0g4y0.deporapp.adaptador.CanchaAdapter
 import io.github.n0g4y0.deporapp.model.Cancha
 import java.util.*
 
 private const val TAG = "ListaCanchaFragment"
 
-class ListaCanchasFragment : Fragment() {
+class ListaCanchasFragment : Fragment(R.layout.fragment_lista_canchas) {
 
-    /*
-   * Interfaz necesaria, para la ACTIVIDAD HOSTING
-   * */
-    interface Callbacks {
-        fun enCanchaSeleccionada(canchaId: UUID)
-    }
+    private val adaptador by lazy { CanchaAdapter() }
 
-    private var callbacks: Callbacks? = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    private lateinit var canchaRecyclerView : RecyclerView
-
-    private var adapter : CanchaAdapter? = CanchaAdapter(emptyList())
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_lista_canchas,container,false)
-
-        canchaRecyclerView = view.findViewById(R.id.cancha_recycler_view) as RecyclerView
-        canchaRecyclerView.layoutManager = LinearLayoutManager(context)
-        canchaRecyclerView.adapter = adapter
-
-        return view
-    }
-
-
-
-
-
-
-    /*
-* to-do el codigo que signifique vincular dentro del proyecto, deberia ser implementado en esta clase HOLDER
-* */
-
-    private inner class CanchaHolder(view: View)
-        : RecyclerView.ViewHolder(view), View.OnClickListener {
-
-        private lateinit var cancha: Cancha
-
-        val tituloTextView: TextView = itemView.findViewById(R.id.cancha_titulo)
-
-        init {
-            itemView.setOnClickListener(this)
-        }
-
-        // funcion que vincula los datos del CRIME, con los WIDGETs
-
-        fun bind(cancha : Cancha){
-
-            this.cancha = cancha
-
-            // asignando el texto, al atributo TEXT de los WIDGETs
-            tituloTextView.text = this.cancha.titulo
-
-        }
-
-        override fun onClick(v: View?) {
-            callbacks?.enCanchaSeleccionada(cancha.id)
-        }
-
-
-    }
-
-    /*
-    * el adapter es un objeto controlador, que se situa entre el RecyclerView, y el conjunto de datos
-    * que desea mostrar del recyclerView.
-    * */
-    private inner class CanchaAdapter(var canchas: List<Cancha>)
-        : RecyclerView.Adapter<CanchaHolder>(){
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
-                : CanchaHolder {
-            val view = layoutInflater.inflate(R.layout.lista_item_cancha,parent,false)
-            return CanchaHolder(view)
-
-        }
-
-        override fun getItemCount() = canchas.size
-
-
-        override fun onBindViewHolder(holder: CanchaHolder, position: Int) {
-
-            val cancha = canchas[position]
-            holder.bind(cancha)
+        adaptador.setItemClickListener {
 
         }
 
 
-    }
 
+
+    }
 
 }
