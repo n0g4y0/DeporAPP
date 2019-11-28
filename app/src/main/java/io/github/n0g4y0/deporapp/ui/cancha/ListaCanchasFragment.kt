fb5aa63f09c0a05a1899b54c803a09281a339053
n0g4y0 <n0g4y0@gmail.com>
2019-11-28 12:21:38 -0400

    rehaciendo la el FRAGMENT lista canchas, y empezando a crear el GRAFICO de NAVEGACION

--- app/src/main/java/io/github/n0g4y0/deporapp/ui/cancha/ListaCanchasFragment.kt
+++ app/src/main/java/io/github/n0g4y0/deporapp/ui/cancha/ListaCanchasFragment.kt
@@ -1,17 +1,14 @@
 package io.github.n0g4y0.deporapp.ui.cancha
 
+
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+
 import io.github.n0g4y0.deporapp.R
 import io.github.n0g4y0.deporapp.adaptador.CanchaAdapter
-import io.github.n0g4y0.deporapp.model.Cancha
-import java.util.*
 
 private const val TAG = "ListaCanchaFragment"
 
