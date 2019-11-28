4942eb783a1c9c1a0ac5f442185a12b90c7d7cde
n0g4y0 <n0g4y0@gmail.com>
2019-11-28 18:17:51 -0400

    agregando mas cambios, ya se muestra el icono, y tambien los datos DUMMYS en el FRAGMENT ANUNCIOS

--- app/src/main/java/io/github/n0g4y0/deporapp/ui/anuncio/AnuncioFragment.kt
+++ app/src/main/java/io/github/n0g4y0/deporapp/ui/anuncio/AnuncioFragment.kt
@@ -6,13 +6,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.navGraphViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import io.github.n0g4y0.deporapp.R
+import io.github.n0g4y0.deporapp.adaptador.AnuncioAdapter
+import io.github.n0g4y0.deporapp.viewmodel.DeporappViewModel
+import kotlinx.android.synthetic.main.fragment_anuncio.*
 
 /**
  * clase diseñada para los anuncios de la app
  */
 class AnuncioFragment : Fragment(R.layout.fragment_anuncio) {
+
+        private val deporappViewModel: DeporappViewModel by navGraphViewModels(R.id.nav_graph)
+
+        private val adaptador by lazy { AnuncioAdapter() }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        anuncios_recycler_view.layoutManager = LinearLayoutManager(context)
+        anuncios_recycler_view.adapter = adaptador
+    }
+
 
 
 
--- app/src/main/java/io/github/n0g4y0/deporapp/ui/cancha/ListaCanchasFragment.kt
+++ app/src/main/java/io/github/n0g4y0/deporapp/ui/cancha/ListaCanchasFragment.kt
@@ -3,27 +3,44 @@ package io.github.n0g4y0.deporapp.ui.cancha
 
 import android.os.Bundle
 import android.view.*
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.navGraphViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import io.github.n0g4y0.deporapp.R
 import io.github.n0g4y0.deporapp.adaptador.CanchaAdapter
+import io.github.n0g4y0.deporapp.viewmodel.DeporappViewModel
+import kotlinx.android.synthetic.main.fragment_lista_canchas.*
 
 private const val TAG = "ListaCanchaFragment"
 
 class ListaCanchasFragment : Fragment(R.layout.fragment_lista_canchas) {
+
+    private val deporappViewModel:DeporappViewModel by navGraphViewModels(R.id.nav_graph)
 
     private val adaptador by lazy { CanchaAdapter() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setHasOptionsMenu(true)
+
         adaptador.setItemClickListener {
 
         }
 
 
+        canchas_recycler_view.layoutManager = LinearLayoutManager(context)
+        canchas_recycler_view.adapter = adaptador
 
 
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.crear_cancha, menu)
     }
 
 }
